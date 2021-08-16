a47889f0b70168f61ed0c1cff68a17fd94262639
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-08-16 17:09:22 +0300

    WIP worker to scrape carn

--- app/workers/daily_package_retrieve_worker.rb
+++ app/workers/daily_package_retrieve_worker.rb
@@ -2,7 +2,25 @@ class DailyPackageRetrieveWorker
   include Sidekiq::Worker
 
   def perform
-    1
-    #TODO: class to scan and spawn package download workers
+    url_to_read = 'https://cran.r-project.org/src/contrib/PACKAGES'
+    read =  URI.open(url_to_read).each do |f|
+      f.each_line do |line|
+        if line.first(8) == "Package:"
+          line.slice!("Package: ")
+          package_name = line.chomp
+        end
+        if line.first(8) == "Version:"
+          line.slice!("Version: ")
+          version = line.chomp
+        end
+        process_name_version(package_name, version)
+      end
+    end
   end
+
+  def process_name_version(package_name, version)
+    #TODO create worker that downloads tar.gz and extracts information,
+      #pass pairs one by one to enque backround jobs
+  end
+
 end
