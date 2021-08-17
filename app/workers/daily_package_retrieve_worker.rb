8641d655f79b08f756a43d20d75a2641b527e7f2
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-08-17 15:13:14 +0300

    Improve daily_package_retrieve_worker and its specs

--- app/workers/daily_package_retrieve_worker.rb
+++ app/workers/daily_package_retrieve_worker.rb
@@ -1,26 +1,28 @@
+require 'open-uri'
 class DailyPackageRetrieveWorker
   include Sidekiq::Worker
 
-  def perform
-    url_to_read = 'https://cran.r-project.org/src/contrib/PACKAGES'
-    read =  URI.open(url_to_read).each do |f|
-      f.each_line do |line|
-        if line.first(8) == "Package:"
-          line.slice!("Package: ")
-          package_name = line.chomp
-        end
-        if line.first(8) == "Version:"
-          line.slice!("Version: ")
-          version = line.chomp
-        end
-        process_name_version(package_name, version)
+  def perform(url_to_read)
+    process_web_file(URI.open(url_to_read))
+  end
+
+  def process_web_file(file)
+    package_name = nil
+    file.each_line do |line|
+      if line.first(8) == "Package:"
+        line.slice!("Package: ")
+        package_name = line.chomp
+      elsif line.first(8) == "Version:" && package_name
+        line.slice!("Version: ")
+        version = line.chomp
+        enque_file_worker(package_name, version)
+        package_name = nil
       end
     end
   end
 
-  def process_name_version(package_name, version)
+  def enque_file_worker(package_name, version)
     #TODO create worker that downloads tar.gz and extracts information,
       #pass pairs one by one to enque backround jobs
   end
-
 end
--- spec/workers/daily_package_retrieve_worker_spec.rb
+++ spec/workers/daily_package_retrieve_worker_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe DailyPackageRetrieveWorker, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   describe '#perform' do 
-    it "returns 1" do
-      expect(subject.perform).to eql(1)
+    it "reads link/file and processes it" do
+      expect(subject).to receive(:process_web_file)
+      subject.perform('spec/support/test_data_set.txt')
+    end
+  end
+
+  describe '#process_web_file' do
+    it 'processes file and calls enque_file_worker with correct args' do
+      file = IO.read('spec/support/test_data_set.txt')
+      expect(subject).to receive(:enque_file_worker).with("RODBC", "1.3-16")
+      subject.process_web_file(file)
+    end
+
+    it 'processes file and calls enque_file_worker 5 times' do
+      file = IO.read('spec/support/test_data_big_set.txt')
+      expect(subject).to receive(:enque_file_worker).exactly(5).times
+      subject.process_web_file(file)
     end
   end
 end
