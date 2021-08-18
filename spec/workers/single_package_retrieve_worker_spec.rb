526f1e356c8dd6bac40ac7960d6b8df5c304dba4
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-08-18 15:57:07 +0300

    despagetify ingle_package_retrieve_worker_spec.rb

--- spec/workers/single_package_retrieve_worker_spec.rb
+++ spec/workers/single_package_retrieve_worker_spec.rb
@@ -1,18 +1,32 @@
 require 'rails_helper'
 
-RSpec.describe SinglePackageRetrieveWorker, type: :model do
+RSpec.describe SinglePackageRetrieveWorker do
   describe '#perform' do 
+    let(:package_name) { 'trust'}
+    let(:version) { '0.1-8' }
+    let(:email) { 'charlie@stat.umn.edu' }
+    let(:persons_name) { 'Charles J. Geyer' }
+    let(:test_file_path) { 'spec/support/trust_0.1-8.tar.gz' }
 
+    subject { described_class.new.perform(package_name, version) }
+    before { allow(URI).to receive(:open).and_return(File.open(test_file_path)) }
 
-    it "downloads and writes package and its author and maintainer" do
-      test_file_path = 'spec/support/trust_0.1-8.tar.gz'
-      allow(URI).to receive(:open).and_return(File.open(test_file_path))
-      subject.perform('trust', '0.1-8')
-      email = 'charlie@stat.umn.edu'
-      name = 'Charles J. Geyer'
-      expect(Package.where(name: 'trust', version: '0.1-8').count).to eql 1
-      expect(Author.where(name: name, email: email).count).to eql 1
-      expect(Maintainer.where(name: name, email: email).count).to eql 1
+    it 'creates package' do
+      expect { subject }.to change {
+        Package.where(name: package_name, version: version).count
+      }.from(0).to(1)
+    end
+
+    it 'creates author' do
+      expect { subject }.to change {
+        Author.where(name: persons_name, email: email).count
+      }.from(0).to(1)
+    end
+
+    it 'creates maintainer' do
+      expect { subject }.to change {
+        Maintainer.where(name: persons_name, email: email).count
+      }.from(0).to(1)
     end
   end
 end
