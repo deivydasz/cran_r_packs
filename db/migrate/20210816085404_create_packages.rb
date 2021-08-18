85f385585b6a203bd6ccd59fbd68289d9dff61e5
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-08-18 14:44:03 +0300

    db/schema.rb

--- db/migrate/20210816085404_create_packages.rb
+++ db/migrate/20210816085404_create_packages.rb
@@ -2,7 +2,7 @@ class CreatePackages < ActiveRecord::Migration[6.1]
   def change
     create_table :packages do |t|
       t.string    :name 
-      t.string    :Version 
+      t.string    :version
       t.datetime  :publication_date 
       t.string    :title 
       t.text      :description 
