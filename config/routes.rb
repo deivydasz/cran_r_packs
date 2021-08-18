07095503470ec8dab424461b8495726e613c4a71
deivydasz <deivydas.ziziliauskas@gmail.com>
2021-08-18 15:53:30 +0300

    Add recurring scheduling, reduce retries to 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+require 'sidekiq/web'
+require 'sidecloq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
   resources :authors
   resources :maintainers
   resources :packages
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
